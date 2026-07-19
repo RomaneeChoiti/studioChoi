fcab3fd7f173db310245d0fecf7ccf14a015cbac
RomaneeChoiti <tmd1568@gmail.com>
2026-07-19 19:17:57 +0900

    -m "chore: remove unused files"

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import "./css/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js";
 import Works from "./pages/Works.js";
-import Blog from "./pages/BlogCard.js";
 import Navbar from "./components/Navbar.js";
 import ProjectDetail from "./pages/ProjectDetailPage.js";
 import Bio from "./pages/Bio";
 import Contact from "./pages/Contact";
 import ArtistStatement from "./pages/ArtistStatement.js";
-import DynamicPostPage from "./components/Post/DynamicPostPage.js";
 import ScrollToTop from "./utils/ScrollToTop.js";
 
 function App() {
@@ -22,12 +20,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/works" element={<Works />} />
             <Route path="/works/:projectId" element={<ProjectDetail />} />
-            <Route path="/blog" element={<Blog />} />
             <Route path="/bio" element={<Bio />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/artist-statement" element={<ArtistStatement />} />
-            <Route path="/:type" element={<DynamicPostPage />} />
-            <Route path="/:type/:postId" element={<DynamicPostPage />} />
           </Routes>
         </div>
       </div>
--- src/utils/calculateReadingTime.js
+++
@@ -1,7 +0,0 @@
-const calculateReadingTime = (content) => {
-  const words = content.split(/\s+/).length;
-  const minutes = Math.ceil(words / 175);
-  return minutes;
-};
-
-export default calculateReadingTime;
